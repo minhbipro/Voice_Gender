c92970c28ace6645ee1aff7d1d4e1e16914fef03
minhbipro <66832698+minhbipro@users.noreply.github.com>
2020-06-12 23:07:41 +0700

    Add files via upload

---
+++ init.R
@@ -0,0 +1,13 @@
+#my_packages = c("RJSONIO", "RCurl", "warbleR", "parallel", "tuneR", "seewave", "gbm", "xgboost", "randomForest", "e1071")
+my_packages = c("RJSONIO", "RCurl", "parallel", "tuneR", "seewave", "gbm", "xgboost", "randomForest", "e1071", "maps", "doParallel", "dtw", "fftw", "foreach", "iterators", "jpeg", "monitoR", "pbapply", "pbmcapply", "proxy", "rjson")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/warbleR/warbleR_1.1.9.tar.gz"
+install.packages(packageurl, repos=NULL, type="source")
